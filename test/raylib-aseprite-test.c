49ec473c3a7fd5702fa6f31c6139400b61cf5bd1
Rob Loach <robloach@gmail.com>
2021-04-11 16:44:07 -0400

    Update

--- test/raylib-aseprite-test.c
+++ test/raylib-aseprite-test.c
@@ -13,18 +13,43 @@ int main(int argc, char *argv[]) {
     TraceLog(LOG_INFO, "================================");
 
     InitWindow(640, 480, "raylib-aseprite-tests");
+    assert(IsWindowReady());
 
     // Make sure we're running in the correct directory.
+    assert(argc > 0);
     const char* dir = GetDirectoryPath(argv[0]);
-    assert(ChangeDirectory(dir) == true);
+    assert(ChangeDirectory(dir));
 
+    // LoadAseprite()
     ase_t* ase = LoadAseprite("resources/backpacker.aseprite");
     assert(ase != NULL);
+    assert(ase->frame_count > 5);
 
+    // TraceAseprite()
+    TraceAseprite(ase);
+
+    // GetAsepriteTexture()
+    Texture texture = GetAsepriteTexture(ase);
+    assert(texture.width > 2);
+    assert(texture.height > 2);
+
+    BeginDrawing();
+    {
+        // DrawAseprite()
+        DrawAseprite(ase, 3, 10, 10, WHITE);
+        // DrawAsepriteV()
+        DrawAsepriteV(ase, 5, (Vector2){10, 20}, WHITE);
+        // DrawAsepriteEx()
+        DrawAsepriteEx(ase, 6, (Vector2){10, 30}, 20, 3, WHITE);
+        // DrawAsepritePro()
+        DrawAsepritePro(ase, 7, (Rectangle){30, 30, 20, 20}, (Vector2){0, 0}, 0.5f, WHITE);
+    }
+    EndDrawing();
+
+    // UnloadAseprite()
     UnloadAseprite(ase);
 
     CloseWindow();
-
     TraceLog(LOG_INFO, "================================");
     TraceLog(LOG_INFO, "raylib-aseprite tests succesful");
     TraceLog(LOG_INFO, "================================");
